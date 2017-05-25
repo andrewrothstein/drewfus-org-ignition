db0b31b585250d8459263724aded0ac5a769672e
Andrew Rothstein <andrew.rothstein@gmail.com>
2017-05-25 17:34:09 -0400

    iterating...

--- drewfus.org/matchbox.js
+++ drewfus.org/matchbox.js
@@ -1,16 +1,35 @@
 {
     "ignition" : { "version": "2.0.0" },
     "storage": {
-        "filesystems": [{
-            "mount": {
-                "device": "/dev/disk/by-label/ROOT",
-                "format": "xfs",
-                "create": {
-                    "force": true,
-                    "options": [ "-L", "ROOT" ]
-                }
-            }
-        }]
+	"disks": [
+	    {
+		"device": "/dev/sda",
+		"wipeTable": true,
+		"partitions": [
+		    {
+			"label": "ROOT",
+			"number": 0,
+			"size": 0,
+			"start": 0
+		    }
+		]
+	    }
+	],
+	"filesystems": [
+	    {
+		"mount": {
+		    "device": "/dev/disk/by-partlabel/ROOT",
+		    "format": "ext4",
+		    "create": {
+			"force": true,
+			"options": [
+			    "-L",
+			    "ROOT"
+			]
+		    }
+		}
+	    }
+	]
     },
     "systemd": {
 	"units": [{
